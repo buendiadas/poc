5216c64b9717609eed9848f20833fe4eae2a519e
Sebastian Siemssen <fubhy@fubhy.com>
2020-07-21 14:44:31 +0200

    feat: add test with snapshot

--- example/tests/richness.test.ts
+++ example/tests/richness.test.ts
@@ -1,17 +1,36 @@
 import { ethers } from 'ethers';
+import { BuidlerProvider } from 'crestproject';
 import { AmIRichAlready } from './contracts/AmIRichAlready';
 import { BasicToken } from './contracts/BasicToken';
 
+async function deploy(provider: BuidlerProvider) {
+  const [deployer, other] = await Promise.all([
+    provider.getSigner(0),
+    provider.getSigner(1),
+  ]);
+
+  const balance = ethers.utils.parseEther('100000000000000');
+  const token = await BasicToken.deploy(deployer, balance);
+  const contract = await AmIRichAlready.deploy(deployer, token.address);
+
+  return {
+    balance,
+    deployer,
+    other,
+    token,
+    contract,
+  };
+}
+
 describe('example', () => {
   it('makes the poor rich', async () => {
-    const [deployer, other] = await Promise.all([
-      provider.getSigner(0),
-      provider.getSigner(1),
-    ]);
-
-    const balance = ethers.utils.parseEther('100000000000000');
-    const token = await BasicToken.deploy(deployer, balance);
-    const contract = await AmIRichAlready.deploy(deployer, token.address);
+    const {
+      balance,
+      contract,
+      token,
+      deployer,
+      other,
+    } = await provider.snapshot(deploy);
 
     const rich = await deployer.getAddress();
     const poor = await other.getAddress();
@@ -32,5 +51,27 @@ describe('example', () => {
     const remaining = balance.sub(richness);
     await expect(token.transfer(poor, remaining)).resolves.toBeReceipt();
     await expect(token.balanceOf(rich)).resolves.toEqBigNumber('0');
+  });
+
+  it('make everyone poor', async () => {
+    const {
+      balance,
+      contract,
+      token,
+      deployer,
+      other,
+    } = await provider.snapshot(deploy);
+
+    const rich = await deployer.getAddress();
+    const poor = await other.getAddress();
+
+    const stranger = '0x0000000000000000000000000000000000000001';
+    await token.transfer(stranger, balance);
+    await expect(token.balanceOf(poor)).resolves.toEqBigNumber(0);
+    await expect(token.balanceOf(rich)).resolves.toEqBigNumber(0);
+
+    const check = contract.check;
+    await expect(check.from(poor).call()).resolves.toBeFalsy();
+    await expect(check.from(rich).call()).resolves.toBeFalsy();
   });
 });
