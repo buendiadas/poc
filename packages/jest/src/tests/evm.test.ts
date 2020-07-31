e42999075ab8757c24c7d789b8ba3d6f2a986b10
Sebastian Siemssen <fubhy@fubhy.com>
2020-07-31 10:28:50 +0200

    test: add tests for mock contract call history

--- packages/jest/src/tests/evm.test.ts
+++ packages/jest/src/tests/evm.test.ts
@@ -9,11 +9,13 @@ async function snapshot(provider: BuidlerProvider) {
   const name = 'Test Token';
   const symbol = 'TEST';
   const token = await ERC20.deploy(signer, name, symbol);
+  const mock = await ERC20.mock(signer);
 
   return {
     deployer,
     someone,
     token,
+    mock,
     name,
     symbol,
   };
@@ -33,6 +35,26 @@ describe('evm', () => {
     const { token, someone } = await provider.snapshot(snapshot);
 
     await expect(token.balanceOf(someone)).resolves.toBeTruthy();
-    expect(token.balanceOf).toHaveBeenCalledOnContractWith(someone);
+    await expect(token.balanceOf).toHaveBeenCalledOnContractWith(someone);
+  });
+
+  it('toHaveBeenCalledOnContract works with mocks', async () => {
+    const { mock, someone } = await provider.snapshot(snapshot);
+
+    await mock.name.returns('mock token');
+    await expect(mock.name()).resolves.toBe('mock token');
+    expect(mock.name).toHaveBeenCalledOnContract();
+
+    await mock.transfer.given(someone, '123').returns(true);
+    await expect(
+      mock.transfer.args(someone, '123').call(),
+    ).resolves.toBeTruthy();
+
+    expect(mock.transfer).toHaveBeenCalledOnContract();
+    await expect(mock.transfer).toHaveBeenCalledOnContractWith(someone, '123');
+
+    await mock.transfer.given(someone, '456').returns(true);
+    await expect(mock.transfer(someone, '456')).resolves.toBeReceipt();
+    await expect(mock.transfer).toHaveBeenCalledOnContractWith(someone, '456');
   });
 });
