411a546e9d422553f9d99fdc0384d085abd3ccda
Sebastian Siemssen <fubhy@fubhy.com>
2020-08-13 14:04:17 +0200

    feat: spawn individual provider instance

--- packages/jest/src/setup/setup-env.ts
+++ packages/jest/src/setup/setup-env.ts
@@ -1,8 +1,68 @@
+import fs from 'fs-extra';
+import path from 'path';
 import NodeEnvironment from 'jest-environment-node';
-import { network } from '@nomiclabs/buidler';
 import { BuidlerProvider } from '@crestproject/evm';
+import {
+  BUIDLEREVM_NETWORK_NAME,
+  SOLC_INPUT_FILENAME,
+  SOLC_OUTPUT_FILENAME,
+} from '@nomiclabs/buidler/internal/constants';
+import { createProvider as createProviderInternal } from '@nomiclabs/buidler/internal/core/providers/construction';
+import {
+  BuidlerNetworkConfig,
+  EthereumProvider,
+  ResolvedBuidlerConfig,
+} from '@nomiclabs/buidler/types';
+import { config } from '@nomiclabs/buidler';
+
+function createProvider(config: ResolvedBuidlerConfig) {
+  const compilerVersion = config.solc.version;
+  const pathConfig = config.paths;
+  const networkName = BUIDLEREVM_NETWORK_NAME;
+  const networkConfig: BuidlerNetworkConfig = {
+    ...(config.networks[networkName] as BuidlerNetworkConfig),
+    // loggingEnabled: true,
+  };
+
+  const provider = createProviderInternal(
+    networkName,
+    networkConfig,
+    compilerVersion,
+    pathConfig,
+  );
+
+  return provider;
+}
+
+async function addCompilationResult(
+  config: ResolvedBuidlerConfig,
+  provider: BuidlerProvider,
+) {
+  const compilerVersion = config.solc.version;
+
+  try {
+    const [compilerInput, compilerOutput] = await Promise.all([
+      fs.readJSON(path.join(config.paths.cache, SOLC_INPUT_FILENAME), {
+        encoding: 'utf8',
+      }),
+      fs.readJSON(path.join(config.paths.cache, SOLC_OUTPUT_FILENAME), {
+        encoding: 'utf8',
+      }),
+    ]);
+
+    return provider.send('buidler_addCompilationResult', [
+      compilerVersion,
+      compilerInput,
+      compilerOutput,
+    ]);
+  } catch (error) {
+    // TODO: Maybe add reporting for this later.
+  }
+}
 
 export default class CrestProjectEnvironment extends NodeEnvironment {
+  public provider?: EthereumProvider;
+
   constructor(config: any) {
     super(config);
   }
@@ -10,10 +70,14 @@ export default class CrestProjectEnvironment extends NodeEnvironment {
   async setup() {
     await super.setup();
 
-    this.global.provider = new BuidlerProvider(network.provider);
+    this.provider = createProvider(config);
+    this.global.provider = new BuidlerProvider(createProvider(config));
+
+    await addCompilationResult(config, this.global.provider);
   }
 
   async teardown() {
+    this.provider?.removeAllListeners();
     // TODO: Collect coverage data here if applicable.
     await super.teardown();
   }
