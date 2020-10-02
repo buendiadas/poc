442d172038a238315f70aeefd180c7c7b29c3a92
Sebastian Siemssen <fubhy@fubhy.com>
2020-10-02 19:01:07 +0200

    feat: add timeout for ganache to stop gracefully

--- packages/ganache/src/preset/setup-env.ts
+++ packages/ganache/src/preset/setup-env.ts
@@ -3,19 +3,27 @@ import ganache from 'ganache-core';
 import { GanacheProvider } from '@crestproject/ganache';
 
 export default class CrestProjectBuidlerEnvironment extends NodeEnvironment {
-  public provider?: ganache.Provider;
+  public ganache?: ganache.Provider;
+  public provider?: GanacheProvider;
 
   async setup() {
     await super.setup();
 
     const config: ganache.IProviderOptions = this.global.ganacheProviderOptions;
-    this.provider = ganache.provider(config);
-    this.global.provider = new GanacheProvider(this.provider);
+    this.ganache = ganache.provider(config);
+    this.provider = new GanacheProvider(this.ganache);
+
+    this.global.provider = this.provider;
   }
 
   async teardown() {
-    if (this.provider) {
-      await new Promise((resolve) => this.provider!.close(() => resolve()));
+    if (this.ganache) {
+      const timeout = this.global.ganacheCloseTimeout ?? 3000;
+
+      await Promise.all([
+        new Promise((resolve) => this.ganache!.close(() => resolve())),
+        new Promise((resolve) => setTimeout(() => resolve(), timeout)),
+      ]);
     }
 
     await super.teardown();
