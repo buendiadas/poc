c02ed3d7013cf073eb8960025462b507045f0d3d
Sebastian Siemssen <fubhy@fubhy.com>
2020-10-31 22:36:31 +0100

    fix: export matchers

--- packages/jest/src/matchers/functions/index.ts
+++ packages/jest/src/matchers/functions/index.ts
@@ -3,3 +3,5 @@ export { toBeRevertedWith } from './toBeRevertedWith';
 export { toBeReceipt } from './toBeReceipt';
 export { toMatchGasSnapshot } from './toMatchGasSnapshot';
 export { toCostLessThan } from './toCostLessThan';
+export { toMatchFunctionInput } from './toMatchFunctionInput';
+export { toMatchFunctionOutput } from './toMatchFunctionOutput';
